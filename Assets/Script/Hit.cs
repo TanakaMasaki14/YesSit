4b4ffc459da6acfc647320fcd6cd8fea294a34cb
タナカ_マサキ <k021g1449@g.neec.ac.jp>
2024-04-22 10:54:28 +0900

    感性をMaxに変更・障害物に当たったらリスポーン仕様に変更
    ゲームオーバーシーン削除

--- Assets/Script/Hit.cs
+++ Assets/Script/Hit.cs
@@ -5,12 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Hit : MonoBehaviour
 {
+    // プレイヤーの初期位置を保存する変数
+    private Vector3 initialPosition;
+
+    // 初期化
+    private void Start()
+    {
+        // プレイヤーの初期位置を保存
+        initialPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // ゴール時の処理
-            SceneManager.LoadScene("GameOver");
+            // プレイヤーの位置を初期位置に戻す
+            other.transform.position = initialPosition;
+
+            // カーソルを表示し、カーソルをロック解除
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
