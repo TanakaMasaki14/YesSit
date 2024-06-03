a04565f09b7f87c1e6b160f5571a0db7c2a93f95
MoritaTsubasa <94036670+MoritaTsubasa@users.noreply.github.com>
2024-06-03 11:19:57 +0900

    ステージ作成途中
    ステージ作成途中

--- Assets/Script/HitT.cs
+++ Assets/Script/HitT.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,24 @@ using UnityEngine.SceneManagement;
 
 public class HitT : MonoBehaviour
 {
-    PlayerMovement playerMovement;
-
     private Vector3 initialPosition;
-
     void Start()
     {
         // プレイヤーの初期位置を保存
-      initialPosition = transform.position;
+        initialPosition = transform.position;
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Object"))
         {
+
+            var impulseSource = GetComponent<CinemachineImpulseSource>();
+            impulseSource.GenerateImpulse();
+
             // プレイヤーの位置を初期位置に戻す
             other.transform.position = initialPosition;
 
-            SceneManager.LoadScene("miyata");
-            // カーソルを表示し、カーソルをロック解除
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene("Tutorial");
         }
     }
 }
---
+++ Assets/Script/SceneChangeClear.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeClear : MonoBehaviour
+{
+    // シーン名を指定
+    public string targetSceneName;
+    // 遷移エフェクト用のスクリプト
+    public Fade fadeController;// フェード制御用のFadeクラスインスタンス
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeController = GetComponent<Fade>();// Fadeクラスのインスタンスを取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // SPACEキーが押されたらシーンを切り替える
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // フェードアウト開始
+            fadeController.FadeOut(1.0f, () =>
+            {
+                SceneManager.LoadScene(targetSceneName);
+            });
+        }
+    }
+}
