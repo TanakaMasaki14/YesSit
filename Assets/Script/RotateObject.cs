bbc044ee5411d7be44f7f8314996b8014cd79f6e
Miyata-0124 <94036166+Miyata-0124@users.noreply.github.com>
2024-07-08 11:03:55 +0900

    Stage4
    中盤

--- Assets/Script/RotateObject.cs
+++ Assets/Script/RotateObject.cs
@@ -25,6 +25,10 @@ public class RotateObject : MonoBehaviour
     [Tooltip("オブジェクトのタイプ")]
     private bool type = false;
 
+    [SerializeField]
+    [Tooltip("オブジェクトの進む向き 1->X 2->Y 3->Z")]
+    private int num = 0;
+
     private Vector3 pos;
     void Start()
     {
@@ -35,6 +39,12 @@ public class RotateObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(num > 3 || num < 1)
+        {
+            //指定した番号以外を当てた場合Xに移動する
+            num = 1;
+        }
+
         if (!type)
         {
             // X,Y,Z軸に対してそれぞれ、指定した角度ずつ回転させている。
@@ -46,8 +56,21 @@ public class RotateObject : MonoBehaviour
             // X,Y,Z軸に対してそれぞれ、指定した角度ずつ回転させている。
             // deltaTimeをかけることで、フレームごとではなく、1秒ごとに回転するようにしている。
             gameObject.transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
-            // X座標にPingPong関数を使い往復するようにしている
-            transform.position = new Vector3(pos.x + Mathf.PingPong(Time.time, moveTime), pos.y, pos.z);
+            if(num == 1)
+            {
+                // X座標にPingPong関数を使い往復するようにしている
+                transform.position = new Vector3(pos.x + Mathf.PingPong(Time.time, moveTime), pos.y, pos.z);
+            }
+            if (num == 2)
+            {
+                // Y座標にPingPong関数を使い往復するようにしている
+                transform.position = new Vector3(pos.x, pos.y + Mathf.PingPong(Time.time, moveTime), pos.z);
+            }
+            if (num == 3)
+            {
+                // Z座標にPingPong関数を使い往復するようにしている
+                transform.position = new Vector3(pos.x, pos.y, pos.z + Mathf.PingPong(Time.time, moveTime));
+            }
         }
     }
 }
